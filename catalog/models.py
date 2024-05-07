932f8b01ddd4562eb4386793c27bf49a1c926b84
alenaodinets <alena.odinecz2013@yandex.ru>
2024-05-07 18:17:33 +0400

    addition blogpost

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 # Create your models here.
 class Product(models.Model):
@@ -62,3 +64,20 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Blogpost(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.CharField(max_length=255, verbose_name="URL", **NULLABLE)
+    content = models.TextField(verbose_name="Контент")
+    preview = models.ImageField(upload_to='catalog/', **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    publication_sign = models.BooleanField(default=True, verbose_name="Опубликовать")
+    number_of_views = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from catalog.models import Product
+from catalog.models import Product, Blogpost
 
 
 class ProdListView(ListView):
@@ -10,6 +10,12 @@ class ProdListView(ListView):
 
 class ProdDetailView(DetailView):
     model = Product
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
 
 
 class ProdCreateView(CreateView):
@@ -23,10 +29,53 @@ class ProdUpdateView(UpdateView):
     fields = ("name", "description", "price", "category", "image_preview")
     success_url = reverse_lazy("catalog:prod_list")
 
+    def get_success_url(self):
+        return reverse("catalog:prod_detail", args=[self.kwargs.get("pk")])
+
 
 class ProdDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy("catalog:prod_list")
+
+
+class BlogPostListView(ListView):
+    model = Blogpost
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(publication_sign=True)
+        return queryset
+
+
+class BlogPostDetailView(DetailView):
+    model = Blogpost
+
+
+class BlogPostCreateView(CreateView):
+    model = Blogpost
+    fields = ("title", "slug", "content", "preview")
+    success_url = reverse_lazy("catalog:blogpost_list")
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+class BlogPostUpdateView(UpdateView):
+    model = Blogpost
+    fields = ("title", "slug", "content", "preview")
+    success_url = reverse_lazy("catalog:blogpost_list")
+
+
+class BlogPostDeleteView(DeleteView):
+    model = Blogpost
+    success_url = reverse_lazy("catalog:blogpost_list")
 
 
 def home(request):
